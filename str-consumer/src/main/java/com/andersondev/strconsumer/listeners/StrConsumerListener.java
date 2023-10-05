b8ff08b5ae623688db76911cc60ade6e8c75bdc0
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-10-04 23:27:10 -0300

    Error handler

--- str-consumer/src/main/java/com/andersondev/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/andersondev/strconsumer/listeners/StrConsumerListener.java
@@ -5,15 +5,18 @@ import org.springframework.stereotype.Component;
 
 import com.andersondev.strconsumer.custom.StrConsumerCustomListener;
 
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
+	@SneakyThrows
 	@StrConsumerCustomListener(groupId = "group-1")
 	public void create(String message) {
 		log.info("CREATE ::: Receive message {}", message);
+		throw new IllegalArgumentException("EXCEPTION...");
 	}
 
 	@StrConsumerCustomListener(groupId = "group-1")
@@ -21,7 +24,7 @@ public class StrConsumerListener {
 		log.info("LOG ::: Receive message {}", message);
 	}
 
-	@KafkaListener(groupId = "group-2",topics = {"str-topic"}, containerFactory = "validMessageContainerFactory")
+	@KafkaListener(groupId = "group-2", topics = { "str-topic" }, containerFactory = "validMessageContainerFactory")
 	public void history(String message) {
 		log.info("HISTORY ::: Receive message {}", message);
 	}
